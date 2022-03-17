2204886dc7e716b517f3504ef6597ae1f69b34e8
ChristianPaulo <christianpaulodev@gmail.com>
2022-03-17 16:25:31 -0300

    create reminder, set title and hour

--- Sources/remindercp/functions.swift
+++ Sources/remindercp/functions.swift
@@ -1,8 +1,15 @@
 //
-//  File.swift
-//  
+//  getInt.swift
+//  Challenge Swift
 //
 //  Created by Christian Paulo on 17/03/22.
 //
 
 import Foundation
+
+func getInt(value : String) -> Int{
+    
+    let e = Int(value) ?? 0
+    return e
+
+}
--- Sources/remindercp/reminder.swift
+++ Sources/remindercp/reminder.swift
@@ -13,35 +13,88 @@ struct Reminder {
     
     static func main() async throws {
         
-        let eventStore = EKEventStore()
         
-        do {
+        print("O que você quer fazer?")
+        print("1 - Adicionar novo alarme")
+        print("2 - Ver meus alarmes")
+        
+        let receberValue = readLine()
+        let valueConverter = getInt(value: receberValue ?? "0")
+        
+        var closingApp = true
+        
+        
+        while(closingApp){
             
-            try await eventStore.requestAccess(to: .event)
+            switch valueConverter{
+                
+            case 1:
+                
+                let eventStore = EKEventStore()
+                
+                do {
+                    
+                    try await eventStore.requestAccess(to: .reminder)
+                    
+                    
+                    
+                    guard let calendar = eventStore.defaultCalendarForNewReminders() else {
+                        
+                        return
+                    }
+                    
+                    //                    Criando o Event Kit para Lembretes
+                    let event = EKReminder(eventStore: eventStore)
+                    
+                    
+                    print("Qual o nome do seu alarme?")
+                    
+                    event.title = readLine()!
+                    
+                    let date = Date()
+                    
+                    
+                    let dateCalendar = Calendar.current
+                    let hourCalendar = dateCalendar.component(.hour, from: date)
+                    let minuteCalendar = dateCalendar.component(.minute, from: date)
+                    //                  Pegar o dia atual
+                    var components = dateCalendar.dateComponents(in: .current, from: date)
+                    
+                    
+                    print("Que horas você quer seu alarme?")
+                    let hours = readLine()!
+                    let separatorHours = hours.components(separatedBy: ":")
+                    components.hour = getInt(value: separatorHours[0])
+                    components.minute = getInt(value: separatorHours[1])
+                    
+                    if components.hour ?? 00 < hourCalendar && components.minute ?? 00 < minuteCalendar {
+                        let dayCalendar = dateCalendar.component(.day, from: date)
+                        components.day = dayCalendar + 1
+                    }
+
+                    //
+                    event.dueDateComponents = components
+                    
+                    
+                    event.calendar = calendar
+                    
+                    try eventStore.save(event, commit: true)
+                    
+                    print("Lembrete Criado")
+                    
+                } catch {
+                    
+                    print(error)
+                }
+                closingApp = false
+                
+            default:
+                print("Você saiu")
+                closingApp = false
+                
+            }
             
-            //            ====Para calendários====
-            //            guard let calendar = eventStore.defaultCalendarForNewEvents else { return }
-            guard let calendar = eventStore.defaultCalendarForNewReminders() else { return }
-            let event = EKReminder(eventStore: eventStore)
-            //            let event = EKEvent(eventStore: eventStore)
-            
-            print("Qual o nome do lembrete?")
-            
-            event.title = readLine()!
-            //            ====Para calendários====
-            //            event.isAllDay = true
-            //            event.startDate = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
-            //            event.endDate = Calendar.current.date(byAdding: .day, value: 5, to: Date())!
-            event.calendar = calendar
-            
-            try eventStore.save(event, commit: true)
-            
-            print("Lembrete Criado")
-            
-        } catch {
-            print(error)
         }
-        
     }
     
 }
